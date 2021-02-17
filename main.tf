0391ee06f1b806715544a10b7f0f5e01e9f81100
Matthew Harrold <github@marrold.co.uk>
2021-02-17 22:06:31 +0000

    Handle tags

--- main.tf
+++ main.tf
@@ -1,31 +1,44 @@
 locals {
    node_groups = {
-    vsps-sip-proxy-1-1 = {
+    sip-proxy-1-1 = {
       desired_capacity = 1
       max_capacity     = 1
       min_capacity     = 1
       instance_type    = "t2.medium"
-      k8s_labels       = {}
-      additional_tags  = {}
+      "k8s_labels" = {
+         "some_label" = "some_value"
+      }
+      "additional_tags" = {
+         "some_tag" = "some_value"
+      }
     }
-    vsps-sip-proxy-1-2 = {
+    sip-proxy-1-2 = {
       desired_capacity = 1
       max_capacity     = 1
       min_capacity     = 1
       instance_type    = "t2.medium"
-      k8s_labels       = {}
-      additional_tags  = {}
+      "k8s_labels" = {
+         "some_label" = "some_value"
+      }
+      "additional_tags" = {
+         "some_tag" = "some_value"
+      }
     }
   }
 
   subnets = {
-     vsps-sip-proxy-1-1 = "some subnet 1"
-     vsps-sip-proxy-1-2 =  "some subnet 2"
+     sip-proxy-1-1 = "some subnet 1"
+     sip-proxy-1-2 =  "some subnet 2"
   }
 
   merged_node_groups = {
      for node_name in  keys(local.node_groups) :
-        node_name => merge(local.node_groups[node_name], {"node_name" = node_name, "subnet" = local.subnets[node_name]})
+        node_name => merge(local.node_groups[node_name], {
+           "node_name" = node_name, 
+           "subnet" = local.subnets[node_name]
+           "k8s_labels" = merge(local.node_groups[node_name].k8s_labels, {"name" = node_name})
+           "additional_tags" = merge(local.node_groups[node_name].additional_tags, {"name" = node_name})
+         })
   }
 
 }
